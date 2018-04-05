ea0cfcf7a5882f21032908d164c5e5766f3c1f45
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-04-05 16:44:22 -0400

    Parse TestDuration out of the result.txt file
    This is part of https://trello.com/c/mChdFZLq/829-tools-add-pbench-stdout-file-parsing-for-misc-metrics-to-pbench-scraper
    The TestDuration object is read from the result.txt file and saved
    in out.json file in output folder.
    
    We have not yet handle the cvs output yet.

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -2,19 +2,24 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"math"
+	"path"
+	"regexp"
 
+	"github.com/openshift/origin/test/extended/cluster/metrics"
 	"github.com/redhat-performance/pbench-analyzer/pkg/result"
 )
 
 // WriteJSON will output all the calculated results to JSON file
-func WriteJSON(resultDir string, hosts []result.Host) error {
+func WriteJSON(resultDir string, r result.Result) error {
 	// Remove NaN as encoding/json doesn't support NaN
-	newHosts := removeNaN(hosts)
+	r.Hosts = removeNaN(r.Hosts)
 
 	// Serialize results as JSON
-	outHosts, err := json.Marshal(newHosts)
+	outHosts, err := json.Marshal(r)
 	if err != nil {
 		return err
 	}
@@ -41,4 +46,52 @@ func removeNaN(hosts []result.Host) []result.Host {
 		}
 	}
 	return newHosts
+}
+
+func GetMetics(searchDir string, m *[]metrics.Metrics) error {
+	resultFilePath := path.Join(path.Dir(path.Clean(searchDir)), "result.txt")
+
+	bytes, err := ioutil.ReadFile(resultFilePath)
+	if err != nil {
+		return err
+	}
+
+	r := regexp.MustCompile(`{[^}]*}`)
+	if err != nil {
+		return err
+	}
+
+	// TODO import this from origin; for now it is still a private field
+	marker_name := "cluster_loader_marker"
+	var bm metrics.BaseMetrics
+	for _, jsonBytes := range r.FindAll(bytes, -1) {
+		jsonString := string(jsonBytes)
+		err := json.Unmarshal([]byte(jsonString), &bm)
+		if err != nil {
+			fmt.Printf("cannot unmarshal the line '%v' for BaseMetrics: %v\n", jsonString, err)
+		}
+
+		if marker_name == bm.Marker {
+			switch bm.Type {
+			case "metrics.TestDuration":
+				var td metrics.TestDuration
+				err := json.Unmarshal([]byte(jsonString), &td)
+				if err != nil {
+					fmt.Printf("cannot unmarshal the line '%v' for TestDuration: %v\n", jsonString, err)
+				}
+				*m = append(*m, td)
+			default:
+				fmt.Printf("unsupported metrics type %v in line: %v\n", bm.Type, jsonString)
+			}
+		} else if err == nil {
+			fmt.Printf("no marker in the line: %v\n", jsonString)
+		}
+	}
+
+	if len(*m) == 0 {
+		return errors.New(fmt.Sprintf("Cannot find metrics in file: %s",
+			resultFilePath))
+	}
+
+	return nil
 }
